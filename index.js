643ecd438d2311d0a9182a3d9e9ddfde0cf5ab2f
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-11-10 07:50:45 -0500

    Add indirect to exports.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 var ajaxHelper = require('./lib/ajax-helper');
 var expectEpic = require('./lib/epic-helper').default;
 
+var indirect = {
+    call:  (fn, ...args) => fn(...args)
+};
+
 exports.MockWebSocket = ajaxHelper.MockWebSocket;
 exports.MockXMLHttpRequest = ajaxHelper.MockXMLHttpRequest;
 exports.expectEpic = expectEpic;
+exports.indirect = indirect;
