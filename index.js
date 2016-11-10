bf8f63e68f8cd2b1af73755168496361fc40299d
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-11-09 19:03:46 -0500

    Adding ExpectEpic

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
 var ajaxHelper = require('./lib/ajax-helper');
+var expectEpic = require('./lib/epic-helper');
+
 exports.MockWebSocket = ajaxHelper.MockWebSocket;
 exports.MockXMLHttpRequest = ajaxHelper.MockXMLHttpRequest;
+exports.ExpectEpic = expectEpic;
---
+++ src/epic-helper.js
@@ -0,0 +1,44 @@
+const expectEpic = (epic, {expected, action, response, call, callArgs, store, done, replace}) => {
+
+    const testScheduler = new TestScheduler((actual, expectation) => {
+
+        // Handle any necessary data manipulation * this could be extended to take an array
+        if (replace && get(actual, replace.key)) {
+            actual[replace.key] = replace.value;
+        }
+
+        setTimeout(() => {
+            /* eslint-disable no-console */
+            //console.log('expected', JSON.stringify(expectation));
+            //console.log('actual--', JSON.stringify(actual));
+            /* eslint-enable no-console */
+            if (done) {
+                actual.should.deep.equal(expectation);
+                done();
+            }
+        });
+
+        if (!done) {
+            actual.should.deep.equal(actual);
+        }
+    });
+
+    const action$ = new ActionsObservable(
+        testScheduler.createHotObservable(...action)
+    );
+    const responseSubs = '^!';
+    const response$ = testScheduler.createColdObservable(...response);
+    call = sinon.stub().returns(response$);
+
+    const test$ = epic(action$, store, call);
+    testScheduler.expectObservable(test$).toBe(...expected);
+    testScheduler.flush();
+
+    if (callArgs !== undefined) {
+        call.called.should.eq(true);
+        call.calledOnce.should.eq(true);
+        call.calledWithExactly(...callArgs).should.eq(true);
+    }
+
+    testScheduler.expectSubscriptions(response$.subscriptions).toBe(responseSubs);
+};
