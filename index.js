29aab980164b2dc91efc77f34215a8d47ffac183
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-11-10 15:59:15 -0500

    Use JSON.stringify to replace values for both actual and expected.

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
-'use strict';
+import Rx from 'rxjs';
+import sinon from 'sinon';
+import {ActionsObservable} from 'redux-observable';
 
-var ajaxHelper = require('./lib/ajax-helper');
-var expectEpic = require('./lib/epic-helper').default;
+const {TestScheduler} = Rx;
 
-var indirect = {
-    call:  (fn, ...args) => fn(...args)
+const expectEpic = (epic, {expected, action, response, call, callArgs, store, done, replace}) => {
+
+    const replacer = (key, value) => {
+        // Filtering out properties
+        if (key === replace.key) {
+            return replace.value;
+        }
+        return value;
+    };
+
+    const replaceValues = (actual, expectation) => {
+        for (var i = 0; i < actual.length; i++) {
+            actual[i] = JSON.parse(JSON.stringify(actual[i], replacer));
+        }
+        for (var j = 0; j < expectation.length; j++) {
+            expectation[j] = JSON.parse(JSON.stringify(expectation[j], replacer));
+        }
+    };
+
+    const testScheduler = new TestScheduler((actual, expectation) => {
+
+
+        setTimeout(() => {
+            // Handle any necessary data manipulation * this could be extended to take an array
+            if (replace) {
+                replaceValues(actual, expectation);
+            }
+            /* eslint-disable no-console */
+            console.log('expected', JSON.stringify(expectation));
+            console.log('actual--', JSON.stringify(actual));
+            /* eslint-enable no-console */
+            if (done) {
+                actual.should.deep.equal(expectation);
+                done();
+            }
+        });
+
+        if (!done) {
+            // Handle any necessary data manipulation * this could be extended to take an array
+            if (replace) {
+                replaceValues(actual, expectation);
+            }
+            actual.should.deep.equal(actual);
+        }
+    });
+
+    const action$ = new ActionsObservable(
+        testScheduler.createHotObservable(...action)
+    );
+    const responseSubs = '^!';
+    const response$ = testScheduler.createColdObservable(...response);
+    call = sinon.stub().returns(response$);
+
+    const test$ = epic(action$, store, call);
+    testScheduler.expectObservable(test$).toBe(...expected);
+    testScheduler.flush();
+
+    if (callArgs !== undefined) {
+        call.called.should.eq(true);
+        call.calledOnce.should.eq(true);
+        call.calledWithExactly(...callArgs).should.eq(true);
+    }
+
+    testScheduler.expectSubscriptions(response$.subscriptions).toBe(responseSubs);
 };
 
-exports.MockWebSocket = ajaxHelper.MockWebSocket;
-exports.MockXMLHttpRequest = ajaxHelper.MockXMLHttpRequest;
-exports.expectEpic = expectEpic;
-exports.indirect = indirect;
+export default expectEpic;
--- src/epic-helper.js
+++ src/epic-helper.js
@@ -1,23 +1,39 @@
 import Rx from 'rxjs';
 import sinon from 'sinon';
 import {ActionsObservable} from 'redux-observable';
-import get from 'lodash/get';
 
 const {TestScheduler} = Rx;
 
 const expectEpic = (epic, {expected, action, response, call, callArgs, store, done, replace}) => {
 
+    const replacer = (key, value) => {
+        // Filtering out properties
+        if (key === replace.key) {
+            return replace.value;
+        }
+        return value;
+    };
+
+    const replaceValues = (actual, expectation) => {
+        for (var i = 0; i < actual.length; i++) {
+            actual[i] = JSON.parse(JSON.stringify(actual[i], replacer));
+        }
+        for (var j = 0; j < expectation.length; j++) {
+            expectation[j] = JSON.parse(JSON.stringify(expectation[j], replacer));
+        }
+    };
+
     const testScheduler = new TestScheduler((actual, expectation) => {
 
-        // Handle any necessary data manipulation * this could be extended to take an array
-        if (replace && get(actual, replace.key)) {
-            actual[replace.key] = replace.value;
-        }
 
         setTimeout(() => {
+            // Handle any necessary data manipulation * this could be extended to take an array
+            if (replace) {
+                replaceValues(actual, expectation);
+            }
             /* eslint-disable no-console */
-            //console.log('expected', JSON.stringify(expectation));
-            //console.log('actual--', JSON.stringify(actual));
+            console.log('expected', JSON.stringify(expectation));
+            console.log('actual--', JSON.stringify(actual));
             /* eslint-enable no-console */
             if (done) {
                 actual.should.deep.equal(expectation);
@@ -26,6 +42,10 @@ const expectEpic = (epic, {expected, action, response, call, callArgs, store, do
         });
 
         if (!done) {
+            // Handle any necessary data manipulation * this could be extended to take an array
+            if (replace) {
+                replaceValues(actual, expectation);
+            }
             actual.should.deep.equal(actual);
         }
     });
